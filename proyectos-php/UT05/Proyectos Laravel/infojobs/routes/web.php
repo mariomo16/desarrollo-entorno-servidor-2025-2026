68f632fcd1946d84490a19a340ce79d6f3aa2075
mariomo16 <mariomo16.cieza@gmail.com>
2025-11-03 14:41:50 +0100

    Infojobs v0.0.1

--- proyectos-php/UT05/Proyectos Laravel/infojobs/routes/web.php
+++ proyectos-php/UT05/Proyectos Laravel/infojobs/routes/web.php
@@ -5,9 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/{id}', function ($id) {
-    return view('welcome', [
-        'id' => $id
-    ]);
-});
