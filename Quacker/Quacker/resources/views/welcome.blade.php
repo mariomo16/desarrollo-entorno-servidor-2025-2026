5c1e76bcadf3f626fd7b8641cb74e9f559acf76b
mariomo16 <mariomo16.cieza@gmail.com>
2025-12-10 21:56:58 +0100

    vista

--- Quacker/Quacker/resources/views/welcome.blade.php
+++ Quacker/Quacker/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Bienvenido a {{ config('app.name') }} 🦆</title>
     @vite(['resources/css/app.css'])
     <style>
         body {
