503449dacaf4a4a5cab1c51eaaab290a1bca3e58
mariomo16 <mariomo16.cieza@gmail.com>
2026-01-11 04:00:55 +0100

    CSS widget auth, y svgs como componentes para limpiar html

--- Quacker/dev/resources/views/components/auth-user-profile.blade.php
+++ Quacker/dev/resources/views/components/auth-user-profile.blade.php
@@ -1,10 +1,15 @@
 {{-- https://laravel.com/docs/12.x/blade#anonymous-components --}}
-<div class="auth-widget">
-    <p>Bienvenido <b>{{ auth()->user()->display_name }}</b> <span
-            class="auth-widget-username">{{ '@' }}{{ auth()->user()->username }}</span></p>
+<div class="auth-user-profile">
+
+    <div class="auth-user-avatar">{{ Str::of(auth()->user()->display_name)->substr(0, 1) }}</div>
+
+    <div class="auth-user-info">
+        <strong>{{ auth()->user()->display_name }}</strong>
+        <span class="text-muted">{{ '@' }}{{ auth()->user()->username }}</span>
+    </div>
 
     <form method="POST" action="{{ route('logout') }}">
         @csrf
-        <button type="submit" class="auth-widget-logout-btn">Cerrar sesión</button>
+        <button type="submit" class="auth-user-logout-btn"><x-icon.logout /></button>
     </form>
 </div>
