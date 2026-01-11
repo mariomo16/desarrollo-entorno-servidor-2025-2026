5197fed0512fd5413c78de7657c94e348cd3df14
mariomo16 <mariomo16.cieza@gmail.com>
2026-01-11 03:00:27 +0100

    Cambiado js a css puro

--- Quacker/dev/resources/js/app.js
+++ Quacker/dev/resources/js/app.js
@@ -1,16 +1,1 @@
 import "./bootstrap";
-
-if (location.href.includes("users")) {
-	document.getElementsByClassName("nav-menu-user")[0].style.color =
-		"var(--color-primary)";
-}
-
-if (location.href.includes("quacks")) {
-	document.getElementsByClassName("nav-menu-quack")[0].style.color =
-		"var(--color-primary)";
-}
-
-if (location.href.includes("quashtags")) {
-	document.getElementsByClassName("nav-menu-quashtag")[0].style.color =
-		"var(--color-primary)";
-}
--- Quacker/dev/resources/views/components/layouts/app.blade.php
+++ Quacker/dev/resources/views/components/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }} / {{ config('app.name') }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css'])
 </head>
 
 <body>
