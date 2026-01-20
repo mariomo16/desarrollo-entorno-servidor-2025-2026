cf5677606c9e9f5ca9749d5aa8ffb6b6b181c745
mariomo16 <mariomo16.cieza@gmail.com>
2026-01-20 01:22:28 +0100

    bloque php

--- Quacker/dev/Quacker/resources/views/components/⚡requack_button.blade.php
+++ Quacker/dev/Quacker/resources/views/components/⚡requack_button.blade.php
@@ -6,26 +6,31 @@ use App\Models\Quack;
 
 new class extends Component {
     public int $quackId;
+    public int $requackCount = 0;
+    public bool $userHasRequacked = false;
 
     public function mount(int $quackId)
     {
         $this->quackId = $quackId;
+        $this->refreshData();
     }
 
     public function requack()
     {
         Quack::find($this->quackId)->requacks()->toggle(Auth::id());
+        $this->refreshData();
+    }
+
+    private function refreshData()
+    {
+        $quack = Quack::with('requacks')->find($this->quackId);
+        $this->requackCount = $quack->requacks()->count();
+        $this->userHasRequacked = $quack->requacks->contains(Auth::id());
     }
 };
 ?>
 
 <div>
-    @php
-        $quack = Quack::with('requacks')->find($quackId);
-        $requackCount = $quack->requacks()->count();
-        $userHasRequacked = $quack->requacks->contains(Auth::id());
-    @endphp
-
     <button wire:click="requack" class="requack-btn">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
             stroke="{{ $userHasRequacked ? '#00BA7C' : 'currentColor' }}" class="size-6">
